f5843133547acce8d66dc27a0b60c0a69f7e9f8d
David C. Bishop <david@davidbishop.org>
2015-10-09 14:33:22 +1100

    Build asynchronously.

--- gowatch.go
+++ gowatch.go
@@ -8,6 +8,8 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
+	"syscall"
 
 	"gopkg.in/fatih/color.v0"
 	"gopkg.in/fsnotify.v1"
@@ -15,31 +17,6 @@ import (
 
 func main() {
 	Main(os.Stdout, os.Stderr)
-}
-
-func runTests(out, eout io.Writer) string {
-	output := bytes.NewBufferString("")
-	ok := color.New(color.Bold, color.FgGreen).SprintFunc()
-	bad := color.New(color.Bold, color.FgRed).SprintFunc()
-
-	cmd := exec.Command("go", "build", "./...")
-	cout, err := cmd.Output()
-	if err != nil {
-		fmt.Fprintln(output, bad("Build ERROR:"), err)
-	} else {
-		fmt.Fprintln(output, ok("Build OK"))
-	}
-
-	cmd = exec.Command("go", "test", "-v", "./...")
-	cout, err = cmd.Output()
-	if err != nil {
-		fmt.Fprintln(output, bad(err))
-	} else {
-		fmt.Fprintln(output, string(cout))
-		fmt.Fprintln(output, ok("Tests OK"))
-	}
-
-	return output.String()
 }
 
 func clear(eout io.Writer) {
@@ -52,12 +29,164 @@ func clear(eout io.Writer) {
 	}
 }
 
-// DoBuild executes a build and test run
-func DoBuild(out, eout io.Writer) {
-	fmt.Fprintln(out, "...")
-	output := runTests(out, eout)
-	clear(eout)
-	fmt.Fprintln(out, output)
+// Builder contains a running building process.
+type Builder struct {
+	buildCmd MyCommand
+	testCmd  MyCommand
+
+	buildOut io.Reader
+	testOut  io.Reader
+}
+
+// Start the build.
+func (builder *Builder) Start() {
+	builder.Kill()
+	builder.buildCmd.Start()
+	builder.testCmd.Start()
+}
+
+// MyCommand stores a command to execute, if it is started again while the last execution is still running it will kill it silently.
+type MyCommand struct {
+	Cmd    *exec.Cmd
+	Lock   sync.Mutex
+	Name   string
+	Args   []string
+	Output chan (CommandResult)
+}
+
+var ok = color.New(color.Bold, color.FgGreen).SprintFunc()
+var bad = color.New(color.Bold, color.FgRed).SprintFunc()
+var refresh = color.New(color.Bold, color.FgWhite).SprintFunc()
+
+// CommandResult stores the result of a completed MyCommand operation.
+type CommandResult struct {
+	output string
+	name   string
+	status string
+}
+
+func (cr *CommandResult) String() string {
+	return cr.name + " " + cr.status + ": " + cr.output
+}
+
+// Start begins executing the command.
+func (mcmd *MyCommand) Start() {
+	mcmd.Kill()
+
+	mcmd.Lock.Lock()
+	go func() {
+		cmd := mcmd.Cmd
+
+		var outBuf bytes.Buffer
+		cmd.Stdout = &outBuf
+
+		err := cmd.Start()
+		mcmd.Lock.Unlock()
+
+		err = cmd.Wait()
+
+		cr := CommandResult{
+			output: outBuf.String(),
+			name:   mcmd.Name,
+			status: ok("✔"),
+		}
+
+		if err != nil {
+			// Don't output anything is the command was killed.
+			if WasKilled(err) {
+				return
+			}
+
+			// Sometimes a command fails with error 1 but works next go, maybe files still being written to disk?
+			if ExitStatus(err) == 1 {
+				return
+			}
+
+			cr.status = bad("✘")
+		}
+
+		mcmd.Output <- cr
+	}()
+}
+
+// WasKilled will check an error as returned by Command.Wait and return true if it was killed.
+func WasKilled(err error) bool {
+	switch e := err.(type) {
+	case *exec.ExitError:
+		switch se := e.Sys().(type) {
+		case syscall.WaitStatus:
+			if se.Signal() == syscall.SIGKILL {
+				return true
+			}
+		default:
+			panic("LINUX ONLY")
+		}
+	}
+	return false
+}
+
+// ExitStatus will check an error as returned by Command.Wait and return the exit code.
+func ExitStatus(err error) int {
+	switch e := err.(type) {
+	case *exec.ExitError:
+		switch se := e.Sys().(type) {
+		case syscall.WaitStatus:
+			return se.ExitStatus()
+		default:
+			panic("LINUX ONLY")
+		}
+	}
+
+	return 0
+}
+
+// Kill the running command.
+func (mcmd *MyCommand) Kill() {
+	{
+		mcmd.Lock.Lock()
+		if mcmd.Cmd != nil && mcmd.Cmd.Process != nil {
+			mcmd.Cmd.Process.Kill()
+		}
+		mcmd.Lock.Unlock()
+	}
+	mcmd.reset()
+}
+
+// Kill the build.
+func (builder *Builder) Kill() {
+	builder.testCmd.Kill()
+	builder.buildCmd.Kill()
+}
+
+func (mcmd *MyCommand) reset() {
+	mcmd.Lock.Lock()
+	defer mcmd.Lock.Unlock()
+	mcmd.Cmd = exec.Command(mcmd.Args[0], mcmd.Args[1:]...)
+}
+
+// NewBuilder make a new builder.
+func NewBuilder() Builder {
+	builder := Builder{}
+
+	builder.buildCmd = MyCommand{
+		Name:   "Build",
+		Args:   []string{"go", "build", "./..."},
+		Output: make(chan CommandResult),
+	}
+
+	builder.testCmd = MyCommand{
+		Name:   "Test",
+		Args:   []string{"go", "test", "-v", "./..."},
+		Output: make(chan CommandResult),
+	}
+
+	return builder
+}
+
+func display(out io.Writer, bRes, tRes CommandResult, bDirty, tDirty bool) {
+	clear(out)
+	fmt.Fprintln(out, bRes.String())
+	fmt.Fprintln(out, tRes.String())
 }
 
 // Main function
@@ -69,32 +198,48 @@ func Main(out io.Writer, eout io.Writer) error {
 
 	done := make(chan bool)
 
-	DoBuild(eout, out)
-	// Process event
+	builder := NewBuilder()
+
+	var bRes CommandResult
+	var tRes CommandResult
+
+	bDirty := true
+	tDirty := true
 	go func() {
 		for {
 			select {
 			case ev := <-watcher.Events:
-				//fmt.Fprintln(out, "event:", ev)
 				if !strings.HasSuffix(ev.Name, ".go") {
 					continue
 				}
-				DoBuild(eout, out)
+				builder.Start()
+
+				bDirty = true
+				tDirty = true
+				tRes.status = refresh("⟳")
+				bRes.status = refresh("⟳")
 			case err := <-watcher.Errors:
 				fmt.Fprintln(eout, "error:", err)
+			case op := <-builder.testCmd.Output:
+				tRes = op
+				tDirty = false
+			case op := <-builder.buildCmd.Output:
+				bRes = op
+				bDirty = false
 			}
+			display(out, bRes, tRes, tDirty, bDirty)
 		}
 	}()
+
+	builder.Start()
 
 	err = watcher.Add(".")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Hang so program doesn't exit
 	<-done
 
-	/* ... do stuff ... */
 	watcher.Close()
 	return nil
 }
